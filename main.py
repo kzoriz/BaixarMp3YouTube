27a1c4d6005f1322f03e9874eb6063bf34382fb6
kzoriz <borisoliveira@alu.uern.br>
2022-08-14 13:42:00 -0300

    Exception commit

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from moviepy import editor as mp
 import re
 import os
 
-#Digite o link do video e o local que deseja salvar o mp3
-link = input("Digite o link do video que deseja salvar: \n")
-path = 'Musicas' # salva na pasta Musica dentro da pasta do seu codigo
-yt = YouTube(link)
-#Começa o Download
-print("Baixando...")
-ys = yt.streams.filter(only_audio=True).first().download(path)
-print("Download Completo!")
+try:
+    #Digite o link do video e o local que deseja salvar o mp3
+    link = input("Digite o link do video que deseja salvar: \n")
+    path = 'Musicas' # salva na pasta Musica dentro da pasta do seu codigo
+    yt = YouTube(link)
+    #Começa o Download
+    print("Baixando...")
+    ys = yt.streams.filter(only_audio=True).first().download(path)
+    print("Download Completo!")
 
-# Converter mp4 para mp3
-for file in os.listdir(path):
-    if re.search('mp4', file):
-        mp4_path = os.path.join(path, file)
-        print(mp4_path)
-        mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
-        new_file = mp.AudioFileClip(mp4_path)
-        new_file.write_audiofile(mp3_path)
-        os.remove(mp4_path)
-print("Sucesso!")
-
+    # Converter mp4 para mp3
+    for file in os.listdir(path):
+        if re.search('mp4', file):
+            mp4_path = os.path.join(path, file)
+            print(mp4_path)
+            mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+            new_file = mp.AudioFileClip(mp4_path)
+            new_file.write_audiofile(mp3_path)
+            os.remove(mp4_path)
+    print("Sucesso!")
+except Exception:
+    print("Algo deu errado!")
+    print("-------------------------------")
+    print("Verifique o link do video\n"
+          "Verifique o nome do diretorio")
+    print("-------------------------------")
